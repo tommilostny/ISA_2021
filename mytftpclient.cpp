2ae3d030bedaed1a0faa2bd84227104f700315a1
tommilostny <tommilostny@live.com>
2021-11-02 15:41:05 +0100

    Separated prompt args parsing from main to func.

--- mytftpclient.cpp
+++ mytftpclient.cpp
@@ -7,43 +7,51 @@
 #include "argumentparser.hpp"
 #include "tftp.hpp"
 
+ArgumentParser* ParsePromptArgs()
+{
+    ArgumentParser* argParser;
+    std::string args;
+
+    std::cout << "> ";
+    std::getline(std::cin, args);
+
+    // Skip and load args again if there are none in stdin.
+    if (args.empty() || std::all_of(args.begin(), args.end(), isspace))
+        return NULL;
+
+    try // Some args loaded, parse them and store in ArgumentParser class properties.
+    {
+        argParser = new ArgumentParser(args);
+    }
+    catch (const std::invalid_argument& exc)
+    {
+        std::cerr << exc.what() << std::endl;
+        return NULL;
+    }
+    // Debugging messages.
+    #ifdef DEBUG
+    std::cout << "Read mode:\t"   << argParser->ReadMode        << std::endl;
+    std::cout << "Write mode:\t"  << argParser->WriteMode       << std::endl;
+    std::cout << "Destination:\t" << argParser->DestinationPath << std::endl;
+    std::cout << "Mode:\t\t"      << argParser->TransferMode    << std::endl;
+    std::cout << "Timeout:\t"     << argParser->Timeout         << std::endl;
+    std::cout << "Size:\t\t"      << argParser->Size            << std::endl;
+    std::cout << "Multicast:\t"   << argParser->Multicast       << std::endl;
+    std::cout << "Address:\t"     << argParser->AddressStr      << std::endl;
+    std::cout << "Port:\t\t"      << argParser->Port            << std::endl;
+    std::cout << "Domain:\t"      << (argParser->Domain == AF_INET ? "AF_INET" : "AF_INET6") << std::endl;
+    #endif
+    return argParser;
+}
+
 int main()
 {
     // Load arguments until end of file (loading file redirected to stdin).
     while (!std::cin.eof())
     {
-        ArgumentParser* argParser;
-        std::string args;
-
-        std::cout << "> ";
-        std::getline(std::cin, args);
-
-        // Skip and load args again if there are none in stdin.
-        if (args.empty() || std::all_of(args.begin(), args.end(), isspace))
+        auto argParser = ParsePromptArgs();
+        if (argParser == NULL)
             continue;
-
-        try // Some args loaded, parse them and store in ArgumentParser class properties.
-        {
-            argParser = new ArgumentParser(args);
-        }
-        catch (const std::invalid_argument& exc)
-        {
-            std::cerr << exc.what() << std::endl;
-            continue;
-        }
-        // Debugging messages.
-        #ifdef DEBUG
-        std::cout << "Read mode:\t"   << argParser->ReadMode        << std::endl;
-        std::cout << "Write mode:\t"  << argParser->WriteMode       << std::endl;
-        std::cout << "Destination:\t" << argParser->DestinationPath << std::endl;
-        std::cout << "Mode:\t\t"      << argParser->TransferMode    << std::endl;
-        std::cout << "Timeout:\t"     << argParser->Timeout         << std::endl;
-        std::cout << "Size:\t\t"      << argParser->Size            << std::endl;
-        std::cout << "Multicast:\t"   << argParser->Multicast       << std::endl;
-        std::cout << "Address:\t"     << argParser->AddressStr      << std::endl;
-        std::cout << "Port:\t\t"      << argParser->Port            << std::endl;
-        std::cout << "Domain:\t"      << (argParser->Domain == AF_INET ? "AF_INET" : "AF_INET6") << std::endl;
-        #endif
 
         Tftp* tftp;
         try //Arguments are loaded correctly, create a new TFTP object and perform transfer.
